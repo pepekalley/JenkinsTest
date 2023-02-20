1d8790bb945ec70d83d282ddc56fd85f6b6568bb
pepekalley <72160033+pepekalley@users.noreply.github.com>
2023-02-20 10:21:51 +0100

    Ajout de Dadado Junior

--- Main.java
+++ Main.java
@@ -2,5 +2,6 @@ public class Main {
   public static void main(String[] args) {
     System.out.println("Hello Bobole !!!");
     System.out.println("Hello Dadado R9 !!!");
+    System.out.println("Hello Dadado R9 Junior; I'm dreaming to see my unborn child !!!");
   }
 }
